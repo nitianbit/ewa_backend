96335c9846c4e7f846b49761aee10e8ac740ec08
Tushar <tushar.mangla1120@gmail.com>
2024-11-30 01:25:02 +0530

    modify auth routes

--- src/modules/auth/routes.js
+++ src/modules/auth/routes.js
@@ -1,16 +1,14 @@
 import express from 'express'
-import { getProfile, login, signup, adminLogin, updateProfile, sendVerificationOTP } from './controller.js';
+import { login, signup, sendOTP, verifyOTP, getProfile } from './controller.js';
 const authRouter = express.Router();
 
 authRouter.post("/login", login)
-authRouter.post("/admin-login", adminLogin)
 authRouter.post("/signup", signup),
-authRouter.get("/send-verification-mail", sendVerificationOTP);
-authRouter.post("/verify", sendVerificationOTP);
-
+authRouter.post("/send-otp", sendOTP),
+authRouter.get("/verify-otp", verifyOTP);
 authRouter.get("/get", getProfile)
-authRouter.put("/update", updateProfile)
 
 export default authRouter;
+
 
 
