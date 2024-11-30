7704964099e61343adf932ddd19c24933dff311b
Tushar <tushar.mangla1120@gmail.com>
2024-11-30 11:13:44 +0530

    availability schema

---
+++ src/db/models/Availability.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import moment from "moment";
+
+const AvailabilitySchema = new mongoose.Schema(
+    {
+      doctor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Doctor',
+        required: true,
+      },
+      day: { type: Number, required: true }, // e.g., "YYYYMMDD"
+      timings: {
+        start: { type: Number, required: true }, 
+        end: { type: Number, required: true },   
+      },
+      createdAt: {
+        type: Number,
+        default: moment().unix(),
+      },
+    updatedAt: { type: Number,  },
+    }
+  );
+  
+  export default Availability = mongoose.model('Availability', AvailabilitySchema);
+  
