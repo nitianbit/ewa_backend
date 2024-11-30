6a1014f00db8949e69cb2552f05c9b3acbe48e60
Tushar <tushar.mangla1120@gmail.com>
2024-11-30 23:47:45 +0530

    added expiresAt

--- src/db/models/Otp.js
+++ src/db/models/Otp.js
@@ -4,7 +4,8 @@ const otpSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Reference to User
     otp: { type: String, required: true }, // OTP value
     target: { type: String,enum: ['phone', 'email'], required: true }, // Phone number or email address
-    createdAt: { type: Date, default: Date.now, expires: 300 }, // Automatically deletes after 5 minutes
+    createdAt: { type: Date }, // Automatically deletes after 5 minutes
+    expireAt: { type: Date, default: Date.now, expires: 3600 }, // Automatically deletes after 5 minutes
     isVerified: { type: Boolean, default: false } // Status of OTP
 }, {
     collection: 'OTP',
