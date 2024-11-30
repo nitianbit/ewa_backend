b4ddc91fa1a1aee9303f9ae39a8631a359032a4b
Tushar <tushar.mangla1120@gmail.com>
2024-11-30 11:18:08 +0530

    common middleware function

--- src/modules/default/utils/helper.js
+++ src/modules/default/utils/helper.js
@@ -1,13 +1,9 @@
- 
-import { Brands } from "../../../db/models/Brands.js";
-import { Category } from "../../../db/models/Category.js";
-import { Products } from "../../../db/models/Products.js";
-import { SubCategory } from "../../../db/models/SubCategory.js";
 import { isValidAdmin, verifyToken } from "../../middlewares/index.js";
 
 export const moduleMiddlewares = {
     'admin': [verifyToken, isValidAdmin],
     'supervisor': [verifyToken],//accessed by admin or supervisor
+    'doctos':[verifyToken]
 }
 
 export const getModule = (module) => {
