5fee4da6a65271588c6d4e95e04af059ae5f7b20
Tushar <tushar.mangla1120@gmail.com>
2024-11-30 11:13:14 +0530

    added appointment model

---
+++ src/db/models/Appointment.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+import moment from "moment";
+
+const AppointmentSchema = new mongoose.Schema(
+  {
+    patient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Patient",
+      required: true,
+    },
+    doctor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Doctor",
+      required: true,
+    },
+    hospital: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Hospital",
+    }, // Optional for individual doctors
+    department: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Department",
+    },
+    appointmentDate: { type: Date, required: true },
+    timeSlot: {
+      start: { type: Number, required: true },  
+      end: { type: Number, required: true },  
+    },
+    status: {
+      type: String,
+      enum: ["Scheduled", "Completed", "Cancelled", "No Show"],
+      default: "Scheduled",
+    },
+    fee: { type: Number, required: true }, // Fee for the appointment
+    paymentStatus: {
+      type: String,
+      enum: ["Paid", "Pending", "Failed"],
+      default: "Pending",
+    },
+    notes: {
+      type: String,
+    }, // Optional notes from the patient
+    createdAt: {
+        type: Number,
+        default: moment().unix(),
+      },
+    updatedAt: { type: Number,  },
+  }
+);
+
+export default Appointment = mongoose.model("Appointment", AppointmentSchema);
