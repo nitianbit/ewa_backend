2d368710585ba1a20c37a5ea1b3cc836d3f5896e
Tushar <tushar.mangla1120@gmail.com>
2024-11-30 11:14:20 +0530

    laboratory schema

---
+++ src/db/models/Laboratories.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import moment from "moment";
+
+const LaboratorySchema = new mongoose.Schema(
+    {
+      name: { type: String, required: true },
+      address: { type: String },
+      contactNumber: { type: String },
+      hospital: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Hospital',
+      }, // Reference to the hospital if affiliated
+      services: [String], // e.g., Blood Test, X-Ray, MRI
+      isIndependent: { type: Boolean, default: true }, // true if not part of a hospital
+      createdAt: {
+        type: Number,
+        default: moment().unix(),
+      },
+    updatedAt: { type: Number,  },
+    }
+  );
+  
+  export default Laboratory = mongoose.model('Laboratory', LaboratorySchema);
+  
