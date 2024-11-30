0160e17a6079cd73788e57a3fc46a50cbf57b02e
Tushar <tushar.mangla1120@gmail.com>
2024-11-30 17:07:57 +0530

    added auth routes

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ import cors from 'cors'
 import { CONFIG } from './src/config/config.js';
 import settings from './settings.js';
 import { connectDB } from './src/db/index.js';
- 
+import authRouter from "./src/modules/auth/routes.js"
+
+
 
 dotenv.config({ path: path.resolve(settings.PROJECT_DIR, `.env`) });
 
@@ -16,10 +18,14 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(express.json({ limit: "10mb" }));
 
-connectDB()
+// app.use("/", routes)
 
- 
+app.use("/api", authRouter)
+app.use("/", (req, res) => {
+    res.send("Welcome to the server!");
+});
+
+connectDB()
 
 app.listen(CONFIG.PORT, () => console.log(`Server running on port ${CONFIG.PORT}`))
 
- 
