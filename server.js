766c981414bd95f8ae8c8aeaea6cd93da75334cf
Tushar <tushar.mangla1120@gmail.com>
2024-12-01 15:49:03 +0530

    added common route method

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import cors from 'cors'
 import { CONFIG } from './src/config/config.js';
 import settings from './settings.js';
 import { connectDB } from './src/db/index.js';
-import authRouter from "./src/modules/auth/routes.js"
+import configureRoutes from './src/routes/index.js';
 
 
 
@@ -18,14 +18,15 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(express.json({ limit: "10mb" }));
 
-// app.use("/", routes)
-
-app.use("/api", authRouter)
-app.use("/", (req, res) => {
-    res.send("Welcome to the server!");
+app.use("/", (req, res, next) => {
+    // res.send("Welcome to the server!");
+    console.log("fdgf")
+    next();
 });
 
 connectDB()
+configureRoutes(app)
 
 app.listen(CONFIG.PORT, () => console.log(`Server running on port ${CONFIG.PORT}`))
+
 
