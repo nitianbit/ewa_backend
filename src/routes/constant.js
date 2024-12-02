e93c62c1af2191107051c521df6816228b4224a7
Tushar <tushar.mangla1120@gmail.com>
2024-12-02 23:21:41 +0530

    base routes

--- src/routes/constant.js
+++ src/routes/constant.js
@@ -1,16 +1,23 @@
 import authRouter from "../modules/auth/routes.js";
+import dashboardRouter from "../modules/dashboard/routes.js";
 import defaultRouter from "../modules/default/routes/index.js";
 
  
 export const routes=[
     {
-        path: '/api',
+        path: '/api/auth',
         router: authRouter,
         middlewares: []
     },
     {
         path: '/api',
         router: defaultRouter,
+        middlewares: []
+    },
+
+    {
+        path: '/api/dashboard',
+        router: dashboardRouter,
         middlewares: []
     },
    
