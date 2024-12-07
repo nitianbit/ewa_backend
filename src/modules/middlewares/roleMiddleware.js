fe70e80894a64ea377aaf38021aca0efe4434912
Tushar <tushar.mangla1120@gmail.com>
2024-12-08 00:57:40 +0530

    added dynamic role wise middleware

---
+++ src/modules/middlewares/roleMiddleware.js
@@ -0,0 +1,32 @@
+import { sendResponse } from '../../utils/helper.js';
+import { decodeToken } from '../auth/middlewares';
+import { getModule } from '../default/utils/helper.js';
+
+const middleware = async (req, res, next, requiredRole, module) => {
+    try {
+
+        const token = req.headers['authorization'];
+        if (!token) return sendResponse(res, 401, "UnAuthorized.");
+
+
+        const decoded = decodeToken(token);
+        if (!decodedData.success) return sendResponse(res, 401, "UnAuthorized.");
+        const Model = getModule(module);
+
+        const user = await Model.findOne({ _id: decoded?.id });
+
+        if (!user || (user.role !== requiredRole)) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        next();
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
+const doctorsMiddleware = (req, res, next) => middleware(req, res, next, 1, "doctors");
+const laboratoriesMiddleware = (req, res, next) => middleware(req, res, next, 1, "laboratories");
+
+module.exports = { middleware, doctorsMiddleware, laboratoriesMiddleware };
