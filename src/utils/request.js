0b761e65c4fbed030f82a9f4464249673c3afa02
Tushar <tushar.mangla1120@gmail.com>
2024-12-22 00:12:42 +0530

    added check for empty query

--- src/utils/request.js
+++ src/utils/request.js
@@ -110,14 +110,19 @@ export const handleGridRequest = async (request, model) => {
     const sort = {};
     sort[gridRequest.sortBy] = gridRequest.sortAsc ? 1 : -1;
     const filters = gridRequest.filters;
-    let query = model.find(filters).sort(sort);
-    if (gridRequest.rows != -1) {
+    let query = await model.find(filters).sort(sort);
+    if (gridRequest.rows != -1 && query.length > 0) {
         const skip = (gridRequest.page - 1) * gridRequest.rows;
         const limit = gridRequest.rows;
         query.skip(skip).limit(limit)
     }
 
-    const rows = await query.exec();
+    let rows;
+
+    if (query.length > 0) {
+        rows = await query.exec();
+    }
+
     let total = null;
     if (gridRequest.page == 1) {
         total = await model.countDocuments(filters);
