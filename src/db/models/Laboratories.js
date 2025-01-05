721958e7ee3497da86bde852251d6d440d8035b9
Tushar <tushar.mangla1120@gmail.com>
2025-01-06 02:35:48 +0530

    changes

--- src/db/models/Laboratories.js
+++ src/db/models/Laboratories.js
@@ -7,13 +7,13 @@ const LaboratorySchema = new mongoose.Schema(
     email: { type: String, unique: true, required: true },
     address: { type: String },
     role: { type: [String] },
-    contactNumber: { type: String },
+    phone: { type: String },
     hospital: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Hospital',
     }, // Reference to the hospital if affiliated
     services: [String], // e.g., Blood Test, X-Ray, MRI
-    isIndependent: { type: Boolean, default: true }, // true if not part of a hospital
+    isIndividual: { type: Boolean, default: true }, // true if not part of a hospital
     createdAt: {
       type: Number,
       default: moment().unix(),
