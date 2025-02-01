7ad143f7a344aadcedd81257a8474089f0d421cc
Shubham <shubham@logicfirst.io>
2025-02-01 08:30:32 +0530

    company wise filter added in dashbaord stats api

--- src/modules/dashboard/controller.js
+++ src/modules/dashboard/controller.js
@@ -7,6 +7,7 @@ import Doctor from "../../db/models/Doctors.js";
 import Company from "../../db/models/Company.js";
 import {sendResponse,} from "../../utils/helper.js";
 import {showError} from "../../utils/logger.js";
+import mongoose from "mongoose";
 
 export const getAppointmentAndEarnings = async (req, res)=>{
   try {
@@ -45,95 +46,107 @@ export const getUsers = async (req, res)=>{
 }
 
 
-export const getDashboardData=async(req,res)=>{
+export const getDashboardData = async (req, res) => {
   try {
-    const todayStart = moment().startOf('day').toDate();
-    const todayEnd = moment().endOf('day').toDate();
-    const thisWeekStart = moment().startOf('week').toDate();
-    const thisWeekEnd = moment().endOf('week').toDate();
-    const thisMonthStart = moment().startOf('month').toDate();
-    const thisMonthEnd = moment().endOf('month').toDate();
+      const { company_id } = req.query; 
 
-    const [
-      totalAppointmentsToday,
-      totalAppointmentsThisWeek,
-      totalAppointmentsThisMonth,
-      totalPatients,
-      totalDoctors,
-      totalCompanies
-  ] = await Promise.all([
-      // Total appointments today
-      Appointment.countDocuments({appointmentDate: { $gte: todayStart, $lte: todayEnd } }),
-      // Total appointments this week
-      Appointment.countDocuments({ appointmentDate: { $gte: thisWeekStart, $lte: thisWeekEnd }}),
-      // Total appointments this month
-      Appointment.countDocuments({ appointmentDate: { $gte: thisMonthStart, $lte: thisMonthEnd }}),
-      // Total patients
-      Patient.countDocuments({}),
-      // Total doctors
-      Doctor.countDocuments({}),
-      // Total companies
-      Company.countDocuments({})
-  ]);
- 
-  return sendResponse(res, 200, "Success", {
-    totalAppointmentsToday,
-    totalAppointmentsThisWeek,
-    totalAppointmentsThisMonth,
-    totalPatients,
-    totalDoctors,
-    totalCompanies
-});
+      const todayStart = moment().startOf('day').toDate();
+      const todayEnd = moment().endOf('day').toDate();
+      const thisWeekStart = moment().startOf('week').toDate();
+      const thisWeekEnd = moment().endOf('week').toDate();
+      const thisMonthStart = moment().startOf('month').toDate();
+      const thisMonthEnd = moment().endOf('month').toDate();
+
+      const matchStage = company_id ? { company: mongoose.Types.ObjectId(company_id) } : {};
+
+      const [
+          totalAppointmentsToday,
+          totalAppointmentsThisWeek,
+          totalAppointmentsThisMonth,
+          totalPatients,
+          totalDoctors,
+          totalCompanies
+      ] = await Promise.all([
+          // Total appointments today
+          Appointment.countDocuments({ ...matchStage, appointmentDate: { $gte: todayStart, $lte: todayEnd } }),
+          // Total appointments this week
+          Appointment.countDocuments({ ...matchStage, appointmentDate: { $gte: thisWeekStart, $lte: thisWeekEnd }}),
+          // Total appointments this month
+          Appointment.countDocuments({ ...matchStage, appointmentDate: { $gte: thisMonthStart, $lte: thisMonthEnd }}),
+          // Total patients
+          Patient.countDocuments(matchStage),
+          // Total doctors
+          Doctor.countDocuments(matchStage),
+          // Total companies
+          company_id ? 1 : Company.countDocuments({})
+      ]);
+
+      return sendResponse(res, 200, "Success", {
+          totalAppointmentsToday,
+          totalAppointmentsThisWeek,
+          totalAppointmentsThisMonth,
+          totalPatients,
+          totalDoctors,
+          totalCompanies
+      });
   } catch (error) {
-    showError(error);
-    return sendResponse(res, 500, "Internal server error", error);
+      showError(error);
+      return sendResponse(res, 500, "Internal server error", error);
   }
-}
+};
 
 export const patientsByCompany = async (req, res) => {
   try {
-      const patientsCompany = await Patient.aggregate([
-          {
-              $group: {
-                  _id: "$company",  
-                  totalPatients: { $sum: 1 }  
-              }
-          },
-          {
-              $lookup: {
-                  from: 'companies',  
-                  localField: '_id',
-                  foreignField: '_id',
-                  as: 'companyDetails'
-              }
-          },
-          {
-              $unwind: "$companyDetails" 
-          },
-          {
-              $project: {
-                  companyName: "$companyDetails.name", 
-                  totalPatients: 1
-              }
-          }
-      ]);
+    const { company_id } = req.query;
+    const matchStage = company_id ? { $match: { company: mongoose.Types.ObjectId(company_id)  } } : { $match: {} };
 
-       const dataForPieChart = patientsCompany.map(item => ({
-          name: item.companyName,
-          value: item.totalPatients
-      }));
+    const patientsCompany = await Patient.aggregate([
+      matchStage,
+      {
+        $group: {
+          _id: "$company",  
+          totalPatients: { $sum: 1 }  
+        }
+      },
+      {
+        $lookup: {
+          from: 'companies',  
+          localField: '_id',
+          foreignField: '_id',
+          as: 'companyDetails'
+        }
+      },
+      {
+        $unwind: "$companyDetails" 
+      },
+      {
+        $project: {
+          companyName: "$companyDetails.name", 
+          totalPatients: 1
+        }
+      }
+    ]);
 
- 
+    const dataForPieChart = patientsCompany.map(item => ({
+      name: item.companyName,
+      value: item.totalPatients
+    }));
+
     return sendResponse(res, 200, "Success", dataForPieChart);
   } catch (error) {
-     showError(error);
-     return sendResponse(res, 500, "Internal server error", error);
+    showError(error);
+    return sendResponse(res, 500, "Internal server error", error);
   }
 };
 
 export const appointmentsByCompany = async (req, res) => {
   try {
+    const { company_id } = req.query; 
+
+    const matchStage = company_id ? { $match: { company: mongoose.Types.ObjectId(company_id) } } : { $match: {} };
+
     const appointments = await Appointment.aggregate([
+      matchStage,
       {
         $group: {
           _id: "$company",  
@@ -159,58 +172,60 @@ export const appointmentsByCompany = async (req, res) => {
       }
     ]);
 
-     const dataForPieChart = appointments.map(item => ({
+    const dataForPieChart = appointments.map(item => ({
       name: item.companyName,
       value: item.totalAppointments
     }));
 
- 
     return sendResponse(res, 200, "Success", dataForPieChart);
   } catch (error) {
-     showError(error);
-     return sendResponse(res, 500, "Internal server error", error);
+    showError(error);
+    return sendResponse(res, 500, "Internal server error", error);
   }
 };
 
 export const patientsByDoctor = async (req, res) => {
   try {
+    const { company_id } = req.query;
+
+    const matchStage = company_id ? { $match: { company: mongoose.Types.ObjectId(company_id) } } : { $match: {} };
+
     const patients = await Patient.aggregate([
+      matchStage,
       {
         $group: {
-          _id: "$doctor", // Group by doctor ID
-          totalPatients: { $sum: 1 } // Count total patients for each doctor
+          _id: "$doctor",  
+          totalPatients: { $sum: 1 }  
         }
       },
       {
         $lookup: {
-          from: 'doctors', // Lookup the doctor details
+          from: 'doctors',  
           localField: '_id',
           foreignField: '_id',
           as: 'doctorDetails'
         }
       },
       {
-        $unwind: "$doctorDetails" // Unwind to get doctor name
+        $unwind: "$doctorDetails" 
       },
       {
         $project: {
-          doctorName: "$doctorDetails.name", // Get doctor name
+          doctorName: "$doctorDetails.name", 
           totalPatients: 1
         }
       }
     ]);
 
-    // Return the data in the format needed for the pie chart
     const dataForPieChart = patients.map(item => ({
       name: item.doctorName,
       value: item.totalPatients
     }));
 
- 
     return sendResponse(res, 200, "Success", dataForPieChart);
   } catch (error) {
-     showError(error);
-     return sendResponse(res, 500, "Internal server error", error);
+    showError(error);
+    return sendResponse(res, 500, "Internal server error", error);
   }
 };
 
