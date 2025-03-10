27a1f412cb1d967ab67330d9b337ef6cc4481602
Shubham <shubham@logicfirst.io>
2025-03-10 23:54:23 +0530

    device token added

--- src/modules/notifications/routes/index.js
+++ src/modules/notifications/routes/index.js
@@ -3,8 +3,8 @@ import express from 'express'
 import { saveDeviceToken } from '../controllers/index.js';
 import { verifyToken } from '../../middlewares/index.js';
 const notificationRouter = express.Router();
- 
-notificationRouter.post('/save',verifyToken, saveDeviceToken);
- 
+
+notificationRouter.post('/save', verifyToken, saveDeviceToken);
+
 
 export default notificationRouter;
