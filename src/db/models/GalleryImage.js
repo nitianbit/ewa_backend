2ca1d1b907d5a11cf2f5ca726537f28879026394
Your Name <nitianbit@gmail.com>
2025-05-10 22:33:39 +0530

    images to image

--- src/db/models/GalleryImage.js
+++ src/db/models/GalleryImage.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const schema = new mongoose.Schema({
     name: String,
     description: { type: String, default: '' },
-    images: { type: String, },  
+    image: { type: String, },  
 }, {
     collection: 'GalleryImages',
 });
